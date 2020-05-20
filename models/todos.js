7013e85287768400a9c740882c0ac76fd24ddfea
Masterxhen <30729644+Masterxhen@users.noreply.github.com>
2020-05-20 09:57:30 +0800

    Data (static db)

---
+++ models/todos.js
@@ -0,0 +1,25 @@
+// {
+//   "todos": [{
+//     "id": 1,
+//     "text": "Wake up"
+//   }, {
+//     "id": 2,
+//     "text": "Play MLBB"
+//   }, {
+//     "id": 3,
+//     "text": "Cook Dinner"
+//   }]
+// }
+
+let todos = [{
+  "id": 1,
+  "text": "Wake up"
+}, {
+  "id": 2,
+  "text": "Play MLBB"
+}, {
+  "id": 3,
+  "text": "Cook Dinner"
+}]
+
+module.exports = todos
